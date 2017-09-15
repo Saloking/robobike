6bc519a6cb55ff7a849df2eae407cddbfab7bc7c
Saloking <lrt86824@stud.uni-stuttgart.de>
2017-09-15 16:34:30 +0200

    little changes on in pendulum_plot.m

--- DataAcquire/pendulum/pendulum_plot.m
+++ DataAcquire/pendulum/pendulum_plot.m
@@ -4,7 +4,7 @@ figure;
 plot(EULERdata.signals.values(:,1))
 
 myData = EULERdata.signals.values(:,1)+180;
-for i=1:1000
+for i=1:length(EULERdata.signals.values)
 if(myData(i)>180)
 myData(i)=myData(i)-360;
 end
